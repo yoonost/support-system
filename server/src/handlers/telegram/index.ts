1158276faf120e3afe2d37332f2b37eda0762690
Sergey Pomazan <86160616+yoonost@users.noreply.github.com>
2024-10-24 18:16:22 +0300

    1.0.5.0.4

--- server/src/handlers/telegram/index.ts
+++ server/src/handlers/telegram/index.ts
@@ -1,8 +1,8 @@
-import TelegramBot, { Message, CallbackQuery } from 'node-telegram-bot-api'
-import { createTicket, getTickets } from './service'
+import TelegramBot, {Message, CallbackQuery, InlineKeyboardButton} from 'node-telegram-bot-api'
+import {closeTicket, createTicket, getTicket, getTickets, sendTicket} from './service'
+import { RowDataPacket } from '../../storage'
 import { isLength } from 'validator'
-import {RowDataPacket} from '../../storage'
-import moment from "moment/moment";
+import moment from 'moment/moment'
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN || '', { polling: true })
 
@@ -24,6 +24,45 @@ const welcomeMessage = async (message: Message): Promise<any> => {
 
 bot.onText(/\/start/, async (message: Message): Promise<any> => {
     return welcomeMessage(message)
+})
+
+bot.onText(/\/ticket_(\d+)/, async (message: Message, match: RegExpExecArray | null): Promise<any> => {
+    if (match) {
+        const { code, data } = await getTicket(message.chat.id, match[1])
+        if (code !== 200) return bot.sendMessage(message.chat.id, `⛔ ${data}`)
+
+        let msg = `📌 #${data.ticket_id} ${data.subject}\n`
+        msg += `🗓️ Created: ${moment(data.created_at * 1000).format("MMM Do YY")} | Last update: ${moment(data.updated_at * 1000).format("MMM Do YY")}\n`
+        msg += `👤 Assigned to: ${data.assigned_id ? (data.assigned_name ? data.assigned_name : `Support agent ${data.assigned_id}`) : 'Not assigned'}\n`
+        msg += (data.status === 1 ? '🟢 Status: Open' : (data.status === 2 ? '🔵 Status: In progress' : '🔴 Status: Closed')) + '\n\n'
+
+        if (data.messages && data.messages.length > 0) {
+            msg += `💬 Messages:\n`
+            data.messages.forEach((ticketMessage: any, index: number) => {
+                const senderMap: { [key: number]: string } = {
+                    1: ticketMessage.sender_name || `#${ticketMessage.sender}`,
+                    2: ticketMessage.sender_name || ticketMessage.sender,
+                    3: ticketMessage.sender_name || `Telegram #${ticketMessage.sender}`
+                }
+                msg += `\n🕒 ${moment(ticketMessage.created_at * 1000).format("MMM Do YY")} - 👤 ${senderMap[ticketMessage.source] || 'unknown'}\n`
+                msg += `📄 ${ticketMessage.message}\n`
+            })
+        } else
+            msg += `💬 No messages in this ticket.\n`
+
+        userState[message.chat.id] = { ...userState[message.chat.id],
+            callback: 'send-ticket',
+            ticket_id: parseInt(match[1])
+        }
+
+        const buttons: InlineKeyboardButton[] = [
+            { text: '👣 Going back', callback_data: 'welcome-message' },
+            data.status !== 3 ? { text: '🔴 Close ticket', callback_data: 'close-ticket' } : null
+        ].filter(Boolean) as InlineKeyboardButton[]
+
+        return await bot.sendMessage(message.chat.id, msg, { reply_markup: { inline_keyboard: [buttons] }})
+    } else
+        await bot.sendMessage(message.chat.id, '⛔ Incorrect command format. Use /ticket_(id), where id is a number.')
 })
 
 bot.on('callback_query', async (query: CallbackQuery): Promise<any> => {
@@ -49,19 +88,27 @@ bot.on('callback_query', async (query: CallbackQuery): Promise<any> => {
 
         tickets.map((ticket: RowDataPacket): void => {
             message += `📌 #${ticket.ticket_id} ${ticket.subject}\n`
-            message += `🗓️ Created: ${moment(ticket.created_at).format("MMM Do YY")} | Last update: ${moment(ticket.updated_at).format("MMM Do YY")}\n`
+            message += `🗓️ Created: ${moment(ticket.created_at * 1000).format("MMM Do YY")} | Last update: ${moment(ticket.updated_at * 1000).format("MMM Do YY")}\n`
             message += `👤 Assigned to: ${ticket.assigned_id ? (ticket.assigned_name ? ticket.assigned_name : `Support agent ${ticket.assigned_id}`) : 'Not assigned'}\n`
             message += (ticket.status === 1 ? '🟢 Status: Open' : (ticket.status === 2 ? '🔵 Status: In progress' : '🔴 Status: Closed')) + '\n'
             if (ticket.last_message !== null) message += `📝 ${ticket.last_message}\n`
-            message += `🔗 /ticket${ticket.ticket_id}\n\n`
+            message += `🔗 /ticket_${ticket.ticket_id}\n\n`
         })
 
         return await bot.sendMessage(query.message.chat.id,
             message,
-            { parse_mode: 'Markdown', reply_markup: { inline_keyboard: [[
+            { reply_markup: { inline_keyboard: [[
                 { text: '👣 Going back', callback_data: 'welcome-message' }
             ]]}
         })
+    }
+
+    if (query.data === 'close-ticket') {
+        const { code, data } = await closeTicket(query.message.chat.id, userState[query.message.chat.id].ticket_id ?? 0)
+        if (code !== 200) return bot.sendMessage(query.message.chat.id, `⛔ ${data}`)
+        return bot.sendMessage(query.message.chat.id, `✔ Ticket #${data} successfully closed`, { reply_markup: { inline_keyboard: [[
+            { text: '👣 Going back', callback_data: 'welcome-message' }
+        ]]}})
     }
 
     return welcomeMessage(query.message)
@@ -95,6 +142,23 @@ bot.on('message', async (message: Message): Promise<any> => {
         await bot.sendMessage(message.chat.id, `🎫 Your ticket #${ticketId} has been successfully created! You will receive a message when the ticket has been processed`, { reply_markup: { inline_keyboard: [[
             { text: '👣 Going back', callback_data: 'welcome-message' }
         ]]}})
-        delete userState[message.chat.id]
+        return delete userState[message.chat.id]
+    }
+
+    if (userState[message.chat.id].callback === 'send-ticket') {
+        if (!isLength(message.text, { min: 1, max: 500 })) {
+            return await bot.sendMessage(message.chat.id, `⛔ Message must be between 1 and 500 characters long`, { reply_markup: { inline_keyboard: [[
+                { text: '👣 Going back', callback_data: 'welcome-message' }
+            ]]}})
+        }
+        const { code, data } = await sendTicket(message.chat.id, userState[message.chat.id].ticket_id ?? 0, message.text)
+        if (code !== 200) return bot.sendMessage(message.chat.id, `⛔ ${data}`)
+        return bot.sendMessage(message.chat.id, `✔ Your message has been delivered`, { reply_markup: { inline_keyboard: [[
+            { text: '👣 Going back', callback_data: 'welcome-message' }
+        ]]}})
     }
 })
+
+export const sendMessage = (userId: string, ticketId: number, message: string): void => {
+    bot.sendMessage(userId, `${message}\n\n/ticket_${ticketId}`)
+}
